ffd9444aad4acd2ad063ff1baa657feaff09824c
Faizan <sh.faizan.ali@hotmail.com>
2012-10-09 18:11:38 +0500

    add topic form validation

--- application/views/topic/add-topic.php
+++ application/views/topic/add-topic.php
@@ -1,37 +1,40 @@
-<?php
-?>
+<?php ?>
 <div class="row">
     <div class="span8">
         <div class="row">
             <div class="span8">
                 <legend>Add a New Topic</legend>
-                <form class="list-form">
+                <form class="list-form add-topic-form" action="http://www.google.com">
+                    <div class="control-group">
+                        <label>Topic</label>
+                        <input class="input-xlarge inputTitle" type="text" placeholder="Type your topic here" />
+                        <span class="help-block"></span>
+                    </div>
+                    
 
-                      <label>Topic</label>
-                      <div class="control-group">
-                          <input class="input-xlarge" type="text" placeholder="Type your topic here">
-                      </div>
-                      <span class="help-block"></span>
 
-                      <label>Description</label>
-                      <div class="control-group">
-                          <textarea rows="6" style="width:271px;" placeholder="Enter the description about your topic"></textarea>
-                      </div>
-                      <span class="help-block"></span>
+                    <div class="control-group">
+                        <label>Description</label>
+                        <textarea rows="6" style="width:271px;" placeholder="Enter the description about your topic" class="inputDescription"></textarea>
+                        <span class="help-block"></span>
+                    </div>
+                    
 
-                      <label>Tags: </label>
-                      <div class="input-append">
+                    <div class="control-group">
+                        <label>Tags: </label>
+                        <div class="input-append">
                             <div class="input-prepend input-append">
-                                <span class="add-on"><i class="icon-tags"></i></span><input class="span4" id="appendedPrependedInput" size="16" type="text" placeholder="Add tags to your list"><button class="btn btn-inverse" type="button">Add</button>
+                                <span class="add-on"><i class="icon-tags"></i></span><input class="span4 inputTags" id="appendedPrependedInput" size="16" type="text" placeholder="Add tags to your list"><button class="btn btn-inverse" id="addTags"type="button">Add</button>
                             </div>
-                      </div>
-                      <span class="help-block"><span class="label">Music</span></span>
+                        </div>
+                        <span class="help-block"></span>    
+                    </div>
+                    <div class="tags-container"></div>
 
-
-                      <button type="submit" class="btn btn-primary form-submit">Add Topic</button>
+                    <button type="submit" class="btn btn-primary form-submit">Add Topic</button>
 
 
                 </form>
             </div>
         </div>
-</div>
+    </div>
--- media/js/scripts.js
+++ media/js/scripts.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
+    count = 0;
+    countLimit = 3;
     $('.login-button').click(function(){
         var addr = $('.login-button').attr('id');
         window.location.href = addr;
     });
     
+    $('#addTags').click(function(){
+        var tagValue = $('.inputTags').val();
+        var tagsHTML = "<span class='"+tagValue+"'><input type='hidden' name='tags[]' value="+tagValue+" class='"+tagValue+"'/> <span class='label label-info'><a href='"+tagValue+"' class='remove-tag'><i class='icon-remove icon-white'></i></a><span class='tag-value'>"+tagValue+"</span></span></span>";
+        $('.tags-container').append(tagsHTML);
+        $('.tags-value').val('');
+    });
+    
+    $('.add-topic-form').submit(function(){
+        var flag = true;
+        if($('.inputTitle').val() == ''){
+            $('.inputTitle').parent('.control-group').addClass('error');
+            $('.inputTitle').siblings('.help-block').html('Title cannot be left blank');
+            flag = false;
+        }
+        if($('.inputDescription').val() == '') {
+            $('.inputDescription').parent('.control-group').addClass('error');
+            $('.inputDescription').siblings('.help-block').html('Title cannot be left blank');
+            flag = false;
+        }
+        return flag;
+    })
 });
 
